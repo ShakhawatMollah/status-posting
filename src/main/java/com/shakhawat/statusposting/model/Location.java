07d33b49939bd8f06a690bbd815cda457d076f92
Md. Shakhawat Hossain Mollah <sh.sumon99@gmail.com>
2020-09-27 00:57:49 +0600

    Location add for tagged in status

---
+++ src/main/java/com/shakhawat/statusposting/model/Location.java
@@ -0,0 +1,38 @@
+package com.shakhawat.statusposting.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "location")
+public class Location {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull
+    private String location;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
