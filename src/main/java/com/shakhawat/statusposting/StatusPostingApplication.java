283456721c10b7e56bbdd83bdde6033934457b41
Md. Shakhawat Hossain Mollah <sh.sumon99@gmail.com>
2020-09-26 16:57:18 +0600

    Project initiated

---
+++ src/main/java/com/shakhawat/statusposting/StatusPostingApplication.java
@@ -0,0 +1,13 @@
+package com.shakhawat.statusposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusPostingApplication.class, args);
+	}
+
+}
