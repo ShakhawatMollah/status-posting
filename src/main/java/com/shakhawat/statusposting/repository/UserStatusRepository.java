23325ce9f28273d0b03d23c8368eace4b559460d
Md. Shakhawat Hossain Mollah <sh.sumon99@gmail.com>
2020-09-27 01:57:30 +0600

    Privacy based Status visible in the web

--- src/main/java/com/shakhawat/statusposting/repository/UserStatusRepository.java
+++ src/main/java/com/shakhawat/statusposting/repository/UserStatusRepository.java
@@ -1,11 +1,18 @@
 package com.shakhawat.statusposting.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.shakhawat.statusposting.model.User;
 import com.shakhawat.statusposting.model.UserStatus;
 
 @Repository
 public interface UserStatusRepository extends JpaRepository<UserStatus, Long>{
+	
+	List<UserStatus> findByIsPublicOrderByIsPinStatusDesc(Boolean isPublic);
+	
+	List<UserStatus>findByCreatedByOrderByIsPinStatusDesc(User createdBy);
 
 }
--- src/main/java/com/shakhawat/statusposting/web/UserController.java
+++ src/main/java/com/shakhawat/statusposting/web/UserController.java
@@ -1,18 +1,28 @@
 package com.shakhawat.statusposting.web;
 
 import com.shakhawat.statusposting.model.User;
+import com.shakhawat.statusposting.model.UserStatus;
 import com.shakhawat.statusposting.repository.UserStatusRepository;
 import com.shakhawat.statusposting.service.SecurityService;
 import com.shakhawat.statusposting.service.UserService;
 import com.shakhawat.statusposting.validator.UserValidator;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
+	
     @Autowired
     private UserService userService;
 
@@ -64,13 +74,31 @@ public class UserController {
     }
     
     @GetMapping("/")
-    public String root() {
-        return "index";
+    public ModelAndView root() {
+    	
+    	ModelAndView mav = new ModelAndView("index");
+    	
+    	mav.addObject("statusList", statusRepository.findByIsPublicOrderByIsPinStatusDesc(Boolean.TRUE));
+		
+        return mav;
     }
 
     @GetMapping({"/dashboard"})
     public String welcome(Model model) {
-    	model.addAttribute("statusList", statusRepository.findAll());
+    	
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetail = (UserDetails) auth.getPrincipal();
+        
+        List<UserStatus> resultSet = statusRepository.findAll();
+        
+        if(!StringUtils.isEmpty(userDetail.getUsername())) {
+            User user = userService.findByUsername(userDetail.getUsername());
+    		if(!StringUtils.isEmpty(user)) {
+    			resultSet = statusRepository.findByCreatedByOrderByIsPinStatusDesc(user);
+    		}
+        }
+		model.addAttribute("statusList", resultSet);
+		
         return "dashboard";
     }
 
